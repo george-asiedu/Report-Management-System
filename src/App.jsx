fe2aa675be28131197057df05190f25c927da974
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-01-13 03:16:09 +0000

    dashboard component route defined

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Registration } from './authentication/Registration';
 import { Login } from './authentication/Login';
+import { Home } from './student-dashboard/home';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route index element={<Registration />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/dashboard' element={<Home />} />
         </Routes>
       </Router>
     </>
