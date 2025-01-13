d206259ab20bb2dedec445f5d6f7e6e191e4b282
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-01-13 03:17:37 +0000

    sidebar component created

---
+++ src/student-dashboard/Sidebar.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import Dashboard from '../assets/dashboard.svg';
+import Logout from '../assets/logout.svg';
+
+export const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
+    return (
+        <>
+            <div
+                className={`${
+                    isSidebarOpen ? "lg:w-[23%]" : "lg:w-[13%]"
+                } bg-primary text-white transition-all duration-300 px-2 h-screen relative z-10`}
+                >
+                <div className="flex items-center justify-between px-4 py-6">
+                    <h1
+                    className={`text-lg font-bold transition-all duration-300 ${
+                        isSidebarOpen ? "opacity-100" : "opacity-0"
+                    }`}>
+                        Dashboard
+                    </h1>
+                    <button
+                        onClick={toggleSidebar}
+                        className="text-white focus:outline-none">
+                        {isSidebarOpen ? "<<" : ">>"}
+                    </button>
+                </div>
+                <nav className="mt-6 flex flex-col">
+                    <ul className="space-y-2">
+                        <li className='flex w-[95%] mx-auto items-center px-2 transition-all duration-300 py-1 gap-4 hover:bg-blue-600 hover:rounded-md'>
+                            <img src={Dashboard} alt="dashboard icon" />
+                            <a
+                                href="#"
+                                className="text-white text-[15px] leading-[1.7rem] tracking-wide">
+                                Home
+                            </a>
+                        </li>
+                        <li className='flex w-[88%] items-center px-2 transition-all duration-300 py-1 gap-4 hover:bg-blue-600 hover:rounded-md absolute left-5 bottom-7'>
+                        <img src={Logout} alt="logout icon" className='w-[22px]' />
+                            <a
+                                href="#"
+                                className="text-white text-[15px] leading-[1.7rem] tracking-wide">
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>      
+        </>
+    )
+}
+
+Sidebar.propTypes = {
+    isSidebarOpen: PropTypes.bool.isRequired,
+    toggleSidebar: PropTypes.func.isRequired,
+};
