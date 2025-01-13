fbc1a51bedd770d78bcfe271a0912c206c52afcb
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-01-13 00:54:13 +0000

    Registration and login components routes defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Registration } from './authentication/Registration';
+import { Login } from './authentication/Login';
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route index element={<Registration />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
     </>
   )
 }
